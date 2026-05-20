c14d535f1ba112231f19c54ff58eb51ad66e9da0
NickCalderone <calderonenick@gmail.com>
2026-05-20 10:49:29 -0600

    added comment for many to many join table

--- properties/models.py
+++ properties/models.py
@@ -16,6 +16,7 @@ class Property(models.Model):
         return self.address
 
 
+# Intermediate model to represent ownership of a property by a user, along with their initial contribution and join date.
 class PropertyOwner(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.PROTECT, related_name="property_ownerships")
